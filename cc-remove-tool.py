7db797c0df2a0acc07e267706b767fb9d55b6bfa
Jaccon <jaccon@Jaccons-iMac.local>
2021-11-21 15:39:14 -0300

    Just start

---
+++ cc-remove-tool.py
@@ -0,0 +1,17 @@
+import os
+
+os.system("echo Remove Photoshop Utilites")
+os.system("rm -rf '/Applications/Adobe Creative Cloud' ")
+
+os.system("echo Remove Photoshop")
+os.system("rm -rf '/Applications/Adobe Photoshop CC 2019' ")
+
+os.system("echo Remove Illustrator")
+os.system("rm -rf '/Applications/Adobe Illustrator CC 2019' ")
+
+os.system("echo Remove Application Support")
+os.system("rm -rf '/Library/Application Support/Adobe' ")
+
+os.system("echo Remove Preferences")
+os.system("rm -rf '/Library/Preferences/com.adobe.*' ")
+
